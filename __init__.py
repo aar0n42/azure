bd5c281355bd2277ecc81ad048a0cce76c6ba6a8
aar0n42 <91214823+aar0n42@users.noreply.github.com>
2021-12-16 17:22:52 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,24 @@
+import logging
+
+import azure.functions as func
+
+import pyodbc
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    server = '<name>.database.windows.net'
+    database = 'mySampleDatabase'
+    username = 'azureuser'
+    password = '{<pw>}'
+    driver= '{ODBC Driver 17 for SQL Server}'
+    strin=""
+    with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT *  FROM SalesLT.Customer")
+            row = cursor.fetchone()
+            while row:
+                strin = strin + " "  + str(row[3])
+                row = cursor.fetchone()
+
+    print(strin)
+    return func.HttpResponse(f"{strin}")
